4d9809f9c69364bbcf0d21f82f205f56d0d5d314
LSH0924 <tngus25866@gmail.com>
2019-07-14 23:08:47 +0900

    컴포넌트 최적화

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -4,6 +4,11 @@ import TodoItem from "../TodoItem";
 
 class TodoList extends Component {
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // 현재 props의 아이템배열과 앞으로 바뀔 예정인 props의 아이템배열이 다를때만 컴포넌트를 업데이트한다.
+    return this.props.children !== nextProps.children;
+  }
+
   render() {
     const {children, onInsert, onToggle, onRemove} = this.props;
     const childrenList = children.map(item => 
