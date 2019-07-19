eafb25666e336a7e9347c6de716106ad633a64e3
LSH0924 <tngus25866@gmail.com>
2019-07-19 22:42:50 +0900

    immutable.js 적용에 맞춰 코드 수정. 일반 객체에서 가져오기 -> Map객체에서 .get 을 사용해 가져오기

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import TodoItem from "../TodoItem";
 
 class TodoList extends Component {
-
+  
   shouldComponentUpdate(nextProps, nextState) {
     // 현재 props의 아이템배열과 앞으로 바뀔 예정인 props의 아이템배열이 다를때만 컴포넌트를 업데이트한다.
     return this.props.children !== nextProps.children;
@@ -13,12 +13,13 @@ class TodoList extends Component {
     const {children, onInsert, onToggle, onRemove} = this.props;
     const childrenList = children.map(item => 
       <TodoItem 
-      key={item.id} 
-      done={item.done} 
-      onInsert={onInsert} 
-      onToggle={()=> onToggle(item.id)}
-      onRemove={()=> onRemove(item.id)}>
-        {item.contents}
+        key={item.get("id")}
+        done={item.get("done")}
+        onInsert={onInsert} 
+        onToggle={() => onToggle(item.get("id"))}
+        onRemove={() => onRemove(item.get("id"))}
+      >
+        {item.get("contents")}
       </TodoItem>
     );
     return (
