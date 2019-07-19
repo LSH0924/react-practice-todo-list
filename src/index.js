dd230d0125e3629205e63eca6b02a199f18de54e
LSH0924 <tngus25866@gmail.com>
2019-07-19 22:34:48 +0900

    14.4.3 스토어 생성, 설정하기

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './styles/main.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 리듀서 적용
+// default로 내보낸 객체의 이름은 import 할 때 마음대로 지정할 수 있다.
+import modules from "./modules";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// 스토어 만들고 리덕스 개발자도구에 연결하기
+const store = createStore(
+    modules,
+    window.devToolsExtension && window.devToolsExtension()
+  );
+
+const renderingComponent = (
+<Provider store={store}>
+    <App />
+</Provider>
+);
+
+ReactDOM.render(renderingComponent, document.getElementById('root'));
 
 serviceWorker.unregister();
