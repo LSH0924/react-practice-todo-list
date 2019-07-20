c716e23696a7e6899a2f0dcc7e7327b8c37402e7
LSH0924 <tngus25866@gmail.com>
2019-07-21 06:24:02 +0900

    loggerMiddleware.js state의 todos리스트 내용이 나오게 수정

--- src/lib/loggerMiddleware.js
+++ src/lib/loggerMiddleware.js
@@ -3,12 +3,14 @@
 // dispatch 는 액션을 처음부터 디스패치 해서 리듀서로 넘겨준다.
 const loggerMiddleware = store => next => action => {
     console.log("현재 store의 state", store.getState());
+    console.log("현재 store의 todos", store.getState().todos.toJS());
     console.log("액션", action);
-
+    
     // 액션을 다음 미들웨어 또는 리듀서로 넘긴다.
     const result = next(action);
-
+    
     console.log("다음 작업으로 넘긴 뒤 store의 state", store.getState());
+    console.log("현재 store의 todos", store.getState().todos.toJS());
     console.log("\n"); // 로그 구분용 newLine
 
     // store.dispatch(ACTION_TYPE) 했을 때(리듀서로 갔을 때)의 결과
