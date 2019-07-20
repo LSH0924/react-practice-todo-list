5f3be5d909b21177f0855597a5584b269790d900
LSH0924 <tngus25866@gmail.com>
2019-07-21 05:55:14 +0900

    15.1.2.2. 로거 미들웨어 생성

---
+++ src/lib/loggerMiddleware.js
@@ -0,0 +1,18 @@
+// next는 store.dispatch와 비슷한 역할을 한다.
+// store.dispatch와 다른 점은 next(action)을 했을 때는 그 다음 처리해야 할 미들웨어로 액션을 넘겨주고, 추가로 처리할 미들웨어가 없으면 바로 리듀서로 넘겨준다는 것.
+// dispatch 는 액션을 처음부터 디스패치 해서 리듀서로 넘겨준다.
+const loggerMiddleware = store => next => action => {
+    console.log("현재 store의 state", store.getState());
+    console.log("액션", action);
+
+    // 액션을 다음 미들웨어 또는 리듀서로 넘긴다.
+    const result = next(action);
+
+    console.log("다음 작업으로 넘긴 뒤 store의 state", store.getState());
+    console.log("\n"); // 로그 구분용 newLine
+
+    // store.dispatch(ACTION_TYPE) 했을 때(리듀서로 갔을 때)의 결과
+    return result;
+};
+
+export default loggerMiddleware;
