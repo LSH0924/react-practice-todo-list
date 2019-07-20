a50b2e6b784c6c433042fea0e62ec84443aa5fdb
LSH0924 <tngus25866@gmail.com>
2019-07-21 06:47:04 +0900

    15.1.2.4. redux-logger 라이브러리 사용

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import modules from "./modules";
-import loggerMiddleware from "./lib/loggerMiddleware";
+// createLogger 는 옵션을 지정해 로거를 생성할 수 있다.
+// 사용하려면 import { createLogger } from "redux-logger";
+// https://github.com/LogRocket/redux-logger#options createLogger에서 사용하는 옵션. 책에 있던 주소가 바뀌었다.
+import logger from "redux-logger";
+
 
 const store = createStore(
     modules,
     compose(
         applyMiddleware(
-            loggerMiddleware
+            logger
         ),
         window.devToolsExtension && window.devToolsExtension()
     )
