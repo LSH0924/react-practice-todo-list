e042b19b8733a1818129b0e7b7734ebe65530cc1
LSH0924 <tngus25866@gmail.com>
2019-08-03 17:53:55 +0900

    16.3.3.2. 라우트 경로에 파라미터 설정 - QueryString

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,16 @@
 import React from "react";
+import queryString from "query-string";
 
-const About = ({match}) => {
+const About = ({location, match}) => {
+    const query = queryString.parse(location.search);
+    console.log(query);
+    
+    const color = {
+        color: query.color
+    }
     return (
         <div>
-            <h2>About</h2>
+            <h2 style={color}>About</h2>
             <p>리액트 라우터 사용하기 실습</p>
             <p>안녕하세요, {match.params.name}의 연습장입니다.</p>
         </div>
