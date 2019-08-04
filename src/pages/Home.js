31be42ba352a6fc83e2ec8b8cf3d9f48875d3e51
LSH0924 <tngus25866@gmail.com>
2019-08-04 23:15:52 +0900

    16.4.3.자바스크립트로 라우팅

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Home = () => {
+const Home = ({history}) => {
     return (
         <div>
             <h2>HOME</h2>
+            <button
+            onClick = {()=>{
+                history.push('/about/Javascript');
+            }}>
+                자바스크립트를 사용해 이동 - about
+            </button>
         </div>
     );
 };
